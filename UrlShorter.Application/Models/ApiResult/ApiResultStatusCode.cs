17f01984b2b9b70b42d1cd4bc020a31c77918651
Reza Ghasemi <reza.ghasemi@amily.de>
2022-12-06 00:47:14 +0100

    Application and Utils

---
+++ UrlShorter.Application/Models/ApiResult/ApiResultStatusCode.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShorter.Application.Models.ApiResult
+{
+    public enum ApiResultStatusCode
+    {
+        [Display(Name = "Success")]
+        Success = 200,
+
+        [Display(Name = "Server Error")]
+        ServerError = 500,
+
+        [Display(Name = "Bad Request Error")]
+        BadRequest = 400,
+
+        [Display(Name = "Not Found")]
+        NotFound = 404,
+
+        [Display(Name = "Empty Error")]
+        ListEmpty = 404,
+
+        [Display(Name = "Process Error")]
+        LogicError = 500,
+
+        [Display(Name = "Authentication Error")]
+        UnAuthorized = 401,
+
+        [Display(Name = "Not Acceptable")]
+        NotAcceptable = 406,
+
+        [Display(Name = "Failed Dependency")]
+        FailedDependency = 424
+    }
+}
---
+++ UrlShorter.Application/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+namespace UrlShorter.Application
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
